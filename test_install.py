51f4274de37d665bbea4fe78c8e08cb323058371
Fredekro000 <nschol22@student.aau.dk>
2026-02-05 13:27:38 +0100

    Update test_install.py
    Created print for each version

--- test_install.py
+++ test_install.py
@@ -5,4 +5,9 @@ import pytest
 import dask
 import matplotlib
 
-print("Hi")
+print("Numpy", numpy.__version__)
+print("Numba", numba.__version__)
+print("Scipy", scipy.__version__)
+print("pytest", pytest.__version__)
+print("dask", dask.__version__)
+print("matplotlib", matplotlib.__version__)
